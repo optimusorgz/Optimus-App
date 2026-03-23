f71ac64c152c45ac4b1a1e9c71ab971b0bbbe887
mekaushikranjan <mritunjaykaushik1803@gmail.com>
2026-03-23 12:44:06 +0530

    code upload

--- server/index.js
+++ server/index.js
@@ -7,8 +7,6 @@ const PORT = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
-
-// Logging middleware
 app.use((req, res, next) => {
     console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
     next();
